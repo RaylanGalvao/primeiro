cc4a4f1f05054fa8bd447deb988c2a41b3aa9f59
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-04-12 09:47:25 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
 
 function App() {
+  const [nome, setNome]=useState('')
+  const [email, setEmail]=useState('')
+  const [idade, setIdade]=useState('')
+
+  const [user,setUser]=useState({})
+
+  function handleRegister(e){
+    e.preventDefault()
+
+
+    alert("usuario registrado com sucesso")
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cadastrando Usuario</h1>
+      <form onSubmit={handleRegister}>
+        <label>Nome:</label>
+        <input 
+        placeholder='Digite seu nome'
+        value={nome}
+        onChange={ (e )=> setNome(e.target.value)}
+        /><br/>
+
+        <label>Email:</label>
+        <input 
+        placeholder='Digite seu Email'
+        value={email}
+        onChange={ (e )=> setEmail(e.target.value)}
+        /><br/>
+
+        <label>Idade:</label>
+        <input 
+        placeholder='Digite seu idade'
+        value={idade}
+        onChange={ (e )=> setIdade(e.target.value)}
+        /><br/>
+
+        <button type='submit'>Registrar</button>
+      </form>
+
+      <br/><br/>
+
+    <div>
+      <sapn>Bem vindo: {user.nome}</sapn><br/>
+      <sapn>Idade {user.idade}:</sapn><br/>
+      <sapn>Email: {user.email}</sapn><br/>
     </div>
-  );
+      </div>
+  )
 }
 
-export default App;
+export default App
